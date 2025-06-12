cc90baeadc35f0441718b72b7e8c1f8411983318
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-12 20:26:34 +0530

    [Feat]: Auth middleware is setup

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  // Format: "Bearer <token>"
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+
+    req.user = user;
+
+    next();
+  });
+};
+
+export default authMiddleware;
