d886147e1286ee7d989247a46c893bfe1facc703
Pratham-fursatAI <prathamd549@gmail.com>
2025-06-12 20:44:04 +0530

    [Feat]: Socket and redis adapter is setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@ import bodyParser from 'body-parser';
 import logger from './utils/logger.js';
 import { PORT } from './config/server-config.js';
 import apiRoutes from './routes/index.js';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { setupSocket } from './sockets/socket.js';
+import { createAdapter } from '@socket.io/redis-streams-adapter'; 
+import { pubClient, subClient } from './config/redis.config.js'; 
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const server = createServer(app);
+
+await pubClient.connect();
+await subClient.connect();
+
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
+io.adapter(createAdapter(pubClient, subClient));
+setupSocket(io);
 
 app.use('/api', apiRoutes);
 
@@ -16,25 +36,27 @@ app.get('/', (req, res) => {
 });
 
 
-const setup_and_start_server=()=>{
-    app.listen(PORT,()=>{
-        logger.info(`Server is running on port ${PORT}`);
-    })
-}
+const setup_and_start_server = () => {
+  server.listen(PORT, () => {
+    logger.info(`Server is running on port ${PORT}`);
+  });
+};
 
 setup_and_start_server();
-const handleServerShutdown=async ()=>{
-    try {
-        logger.info("Shutting down server...");
-        logger.info("Server shutdown complete.");
-        await mongoose.connection.close();
-        logger.info("Database connection closed.");
-        process.exit(0);
-    } catch (error) {
-        logger.error("Error during server shutdown:", error);
-        process.exit(1);
-    }
-}
+
+
+const handleServerShutdown = async () => {
+  try {
+    logger.info('Shutting down server...');
+    await pubClient.quit();
+    await subClient.quit();
+    logger.info('Server shutdown complete.');
+    process.exit(0);
+  } catch (error) {
+    logger.error('Error during server shutdown:', error);
+    process.exit(1);
+  }
+};
 
 process.on('SIGINT', handleServerShutdown);
 process.on('SIGTERM', handleServerShutdown);
